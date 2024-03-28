2d4bda79ccf00ab9e5cbbb8675579c7969250c41
손설빈 <92971809+seolbb@users.noreply.github.com>
2024-03-28 17:42:09 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,17 +1,12 @@
 class Solution {
     public String solution(int num) {
         String answer = "";
-        if(num % 2 == 0){
-            answer = "Even";
-        } else {
+        
+        if(num % 2 != 0){
             answer = "Odd";
+        } else {
+            answer = "Even";
         }
         return answer;
-    }
-    
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        
-        System.out.println("이게 맞아?");
     }
 }
